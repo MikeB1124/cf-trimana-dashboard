6b39ec6ef2de7d81d7c71ad63d66ef4235f4dad0
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2024-05-15 19:25:56 -0700

    basic api gateway resource

---
+++ api.py
@@ -0,0 +1,32 @@
+from stacker.blueprints.base import Blueprint
+from troposphere import (
+    Output,
+    Ref,
+    s3,
+    apigateway
+)
+
+
+class Trimana(Blueprint):
+
+    def create_api_gateway(self):
+        t = self.template
+
+        api = apigateway.RestApi(
+            "TrimanaDashboardApi",
+            Name="trimana-dashboard-api-gateway",
+            ApiKeySource="HEADER",
+        )
+
+        t.add_resource(api)
+
+        t.add_output(
+            Output(
+                "TrimanaDashboardApiId",
+                Value=Ref(api),
+            )
+        )
+
+    def create_template(self):
+        self.create_api_gateway()
+        return self.template
